a592c0c633d2fec73a5a11f980c57552298795f3
lccmorales <lccmorales@gmail.com>
2022-05-18 14:45:49 -0700

    Ejercicio hasta helpers

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,35 +1,30 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types';
+import { GifGridItem } from './GifGridItem';
+import { getGifs } from '../helpers/getGifs';
 
-export const GifGrid = ( {category} ) => {
+export const GifGrid = ( { category } ) => {
 
-    const [count, setCount] = useState(0);
+    const [images, setImages] = useState([]);
 
     useEffect( () => {
-        getGifs();
-    }, []);
-
-    const getGifs = async() => {
-        const url = 'https://api.giphy.com/v1/gifs/search?q=Arcane&limit=10&api_key=wGFpRizoH8FkJI4Dbmue2wSoctrFUqGq';
-        const resp = await fetch ( url );
-        const { data } = await resp.json();
-
-        const gifs = data.map( img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        });
-
-        console.log(gifs);
-    }
+        getGifs( category )
+            .then( setImages );
+    }, [ category ]);
 
     return (
         <>
-            <h3>{ category }</h3>
-            <h1>{ count }</h1>
-            <button onClick = { () => setCount(count + 1) }>+1</button>
+        <h3>{ category }</h3>
+        <div className='card-grid'>
+            {
+                images.map( img => (
+                    <GifGridItem
+                        key={ img.id }
+                        { ...img }
+                    />
+                ))
+            }
+        </div>
         </>
     )
 }
