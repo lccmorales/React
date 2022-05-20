4b5971fdb2f0131278f353124d37d1ce60cc2cd2
lccmorales <lccmorales@gmail.com>
2022-05-20 11:41:37 -0700

    Docs agregado

---
+++ 04-gif-expert-app/docs/precache-manifest.214f739a5a6f4c4f742f490f93b9c680.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb22400e2049a5b5f3a2576975f43693",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9776dfd65843bba65aaf",
+    "url": "/static/css/main.00d0dfe3.chunk.css"
+  },
+  {
+    "revision": "2aaae38fbcb0ad5c6be9",
+    "url": "/static/js/2.e1449281.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e1449281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9776dfd65843bba65aaf",
+    "url": "/static/js/main.850679d4.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
