edef56b8e8059d1f94c1db83ac77d734da76cd2c
lccmorales <lccmorales@gmail.com>
2022-06-06 15:54:26 -0700

    useRef ejemplos

---
+++ 05-hook-app/src/components/04-useRef/FocusScreen.js
@@ -0,0 +1,31 @@
+import React, { useRef } from 'react';
+import '../02-useEffect/effects.css';
+
+export const FocusScreen = () => {
+    const inputRef = useRef();
+
+    const handleClick = () => {
+        inputRef.current.select();
+    }
+
+    return (
+    <div>
+        <h1>FocusScreen</h1>
+        <hr />
+
+        <input
+            ref={ inputRef }
+            type='text'
+            className='form-control'
+            placeholder='Jesus'
+        />
+
+        <button
+            className='btn btn-outline-primary mt-5'
+            onClick={ handleClick }
+        >
+            Focus
+        </button>
+    </div>
+    )
+}
