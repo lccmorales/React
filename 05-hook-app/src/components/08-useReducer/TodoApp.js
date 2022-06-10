7b4c80b56b761e96dcbc69fa0842dd4d8d0c1470
lccmorales <lccmorales@gmail.com>
2022-06-09 18:57:43 -0700

    intro todo list

--- 05-hook-app/src/components/08-useReducer/TodoApp.js
+++ 05-hook-app/src/components/08-useReducer/TodoApp.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import { todoReducer } from './todoReducer';
+
 import './styles.css';
 
+const initialState = [{
+    id: new Date().getTime(),
+    desc: 'Aprender React',
+    done: false
+}]
+
 export const TodoApp = () => {
+    const [ todos ] = useReducer( todoReducer, initialState );
+    console.log(todos);
     return (
     <div>
-        <h1>TodoApp</h1>
+        <h1>TodoApp ( { todos.length } )</h1>
         <hr />
+
+        <div className='row'>
+            <div className='col-7'>
+                <ul className='list-group list-group-flush'>
+                {
+                    todos.map( (todo, i) => (
+                        <li className='list-group-item'
+                            key={ todo.id }
+                        >
+                            <p className='text-center'>{ i +1 }. { todo.desc }</p>
+                            <button className='btn btn-danger'>Borrar</button>
+                        </li>
+                    ))
+                }
+                </ul>
+            </div>
+            <div className='col-5'>
+                <h4>Agregar TODO</h4>
+                <hr />
+                <form>
+                    <input type='text' className='form-control' name='description' placeholder='Aprender...' autoComplete='off' />
+                    <button className='btn btn-outline-primary mt-1 w-100'>
+                        Agregar
+                    </button>
+                </form>
+            </div>
+        </div>
 
     </div>
     )
