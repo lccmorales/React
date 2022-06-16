600ea63bfca258362ef02ca6c87e8c31cc182cfb
lccmorales <lccmorales@gmail.com>
2022-06-15 19:58:51 -0700

    Con la actualizacion del curso volvere a verlo practicamente, solo que nada mas realizare los ejercicios importantes, en este caso Seccion 4 fue el CounterApp

---
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const CounterApp = ({ value }) => {
+    const [ counter, setCounter ] = useState( value );
+
+    //handleReset
+    const handleReset = () => setCounter ( value );
+    
+    //handleSub
+    const handleSub = () => setCounter ( counter - 1 );
+    
+    //handleAdd
+    const handleAdd = () => {
+        setCounter ( counter + 1 );
+        // setCounter((c) => c + 1);
+    }
+
+    return (
+      <>
+        <h1>CounterApp</h1>
+        <h2> { counter } </h2>
+        <button onClick = { handleSub } >-1</button>
+        <button onClick = { handleReset } >Reset</button>
+        <button onClick = { handleAdd } >+1</button>
+      </>
+  )
+}
+
+CounterApp.propTypes = {
+    value: PropTypes.number.isRequired
+}
+
+CounterApp.defaultProps = {
+    value: 25
+}
+
+export default CounterApp;
